f66418ca64dea3932becbecbec5695a8aa45819a
Varun Narravula <varun@snare.dev>
2024-09-15 20:09:26 -0700

    chore: update nix flake and database secret

--- config/server.nix
+++ config/server.nix
@@ -5,7 +5,16 @@
   ...
 }: let
   chai = inputs.chai-backend.packages.${pkgs.system}.default;
+  port = "6969";
 in {
+  # Reverse proxy and HTTPS certs
+  services.caddy = {
+    enable = true;
+    virtualHosts."api.chai-solutions.org".extraConfig = ''
+      reverse_proxy 127.0.0.1:${port}
+    '';
+  };
+
   systemd.services.chaid = {
     description = "Chai Solutions API server daemon";
     restartIfChanged = true;
